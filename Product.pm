b4b8d379ade7617db8f24687fc316f889cb15c8c
trodevel <trodevel@gmail.com>
2021-01-24 03:51:37 +0100

    added return value to merge()

--- Product.pm
+++ Product.pm
@@ -198,44 +198,189 @@ sub merge($)
 
     die "mismatching handle" if ( $self->{handle} ne $obj_ref->{handle} );
 
-    $self->{title} = $obj_ref->{title};
-    $self->{body_html} = $obj_ref->{body_html};
-    $self->{vendor_min_2_characters} = $obj_ref->{vendor_min_2_characters};
-    $self->{type} = $obj_ref->{type};
-    $self->{tags} = $obj_ref->{tags};
-    $self->{published} = $obj_ref->{published};
-    $self->{option1_name} = $obj_ref->{option1_name};
-    $self->{option1_value} = $obj_ref->{option1_value};
-    $self->{option2_name} = $obj_ref->{option2_name};
-    $self->{option2_value} = $obj_ref->{option2_value};
-    $self->{option3_name} = $obj_ref->{option3_name};
-    $self->{option3_value} = $obj_ref->{option3_value};
-    $self->{variant_sku} = $obj_ref->{variant_sku};
-    $self->{variant_grams} = $obj_ref->{variant_grams};
-    $self->{variant_inventory_tracker} = $obj_ref->{variant_inventory_tracker};
-    $self->{variant_inventory_qty} = $obj_ref->{variant_inventory_qty};
-    $self->{variant_inventory_policy} = $obj_ref->{variant_inventory_policy};
-    $self->{variant_fulfillment_service} = $obj_ref->{variant_fulfillment_service};
-    $self->{variant_price} = $obj_ref->{variant_price};
-    $self->{variant_compare_at_price} = $obj_ref->{variant_compare_at_price};
-    $self->{variant_requires_shipping} = $obj_ref->{variant_requires_shipping};
-    $self->{variant_taxable} = $obj_ref->{variant_taxable};
-    $self->{variant_barcode} = $obj_ref->{variant_barcode};
+    my $is_modified = 0;
+
+    if( $self->{title} ne $obj_ref->{title} )
+    {
+        $self->{title} = $obj_ref->{title};
+        $is_modified = 1;
+    }
+
+    if( $self->{body_html} ne $obj_ref->{body_html} )
+    {
+        $self->{body_html} = $obj_ref->{body_html};
+        $is_modified = 1;
+    }
+    if( $self->{vendor_min_2_characters} ne $obj_ref->{vendor_min_2_characters} )
+    {
+        $self->{vendor_min_2_characters} = $obj_ref->{vendor_min_2_characters};
+        $is_modified = 1;
+    }
+    if( $self->{type} ne $obj_ref->{type} )
+    {
+        $self->{type} = $obj_ref->{type};
+        $is_modified = 1;
+    }
+    if( $self->{tags} ne $obj_ref->{tags} )
+    {
+        $self->{tags} = $obj_ref->{tags};
+        $is_modified = 1;
+    }
+    if( $self->{published} ne $obj_ref->{published} )
+    {
+        $self->{published} = $obj_ref->{published};
+        $is_modified = 1;
+    }
+    if( $self->{option1_name} ne $obj_ref->{option1_name} )
+    {
+        $self->{option1_name} = $obj_ref->{option1_name};
+        $is_modified = 1;
+    }
+    if( $self->{option1_value} ne $obj_ref->{option1_value} )
+    {
+        $self->{option1_value} = $obj_ref->{option1_value};
+        $is_modified = 1;
+    }
+    if( $self->{option2_name} ne $obj_ref->{option2_name} )
+    {
+        $self->{option2_name} = $obj_ref->{option2_name};
+        $is_modified = 1;
+    }
+    if( $self->{option2_value} ne $obj_ref->{option2_value} )
+    {
+        $self->{option2_value} = $obj_ref->{option2_value};
+        $is_modified = 1;
+    }
+    if( $self->{option3_name} ne $obj_ref->{option3_name} )
+    {
+        $self->{option3_name} = $obj_ref->{option3_name};
+        $is_modified = 1;
+    }
+    if( $self->{option3_value} ne $obj_ref->{option3_value} )
+    {
+        $self->{option3_value} = $obj_ref->{option3_value};
+        $is_modified = 1;
+    }
+    if( $self->{variant_sku} ne $obj_ref->{variant_sku} )
+    {
+        $self->{variant_sku} = $obj_ref->{variant_sku};
+        $is_modified = 1;
+    }
+    if( $self->{variant_grams} != $obj_ref->{variant_grams} )
+    {
+        $self->{variant_grams} = $obj_ref->{variant_grams};
+        $is_modified = 1;
+    }
+    if( $self->{variant_inventory_tracker} ne $obj_ref->{variant_inventory_tracker} )
+    {
+        $self->{variant_inventory_tracker} = $obj_ref->{variant_inventory_tracker};
+        $is_modified = 1;
+    }
+    if( $self->{variant_inventory_qty} ne $obj_ref->{variant_inventory_qty} )
+    {
+        $self->{variant_inventory_qty} = $obj_ref->{variant_inventory_qty};
+        $is_modified = 1;
+    }
+    if( $self->{variant_inventory_policy} ne $obj_ref->{variant_inventory_policy} )
+    {
+        $self->{variant_inventory_policy} = $obj_ref->{variant_inventory_policy};
+        $is_modified = 1;
+    }
+    if( $self->{variant_fulfillment_service} ne $obj_ref->{variant_fulfillment_service} )
+    {
+        $self->{variant_fulfillment_service} = $obj_ref->{variant_fulfillment_service};
+        $is_modified = 1;
+    }
+    if( $self->{variant_price} != $obj_ref->{variant_price} )
+    {
+        $self->{variant_price} = $obj_ref->{variant_price};
+        $is_modified = 1;
+    }
+    if( $self->{variant_compare_at_price} ne $obj_ref->{variant_compare_at_price} )
+    {
+        $self->{variant_compare_at_price} = $obj_ref->{variant_compare_at_price};
+        $is_modified = 1;
+    }
+    if( $self->{variant_requires_shipping} ne $obj_ref->{variant_requires_shipping} )
+    {
+        $self->{variant_requires_shipping} = $obj_ref->{variant_requires_shipping};
+        $is_modified = 1;
+    }
+    if( $self->{variant_taxable} ne $obj_ref->{variant_taxable} )
+    {
+        $self->{variant_taxable} = $obj_ref->{variant_taxable};
+        $is_modified = 1;
+    }
+    if( $self->{variant_barcode} ne $obj_ref->{variant_barcode} )
+    {
+        $self->{variant_barcode} = $obj_ref->{variant_barcode};
+        $is_modified = 1;
+    }
     if( index( $self->{image_src}, "cdn.shopify.com" ) == -1 )
     {
-        $self->{image_src} = $obj_ref->{image_src};
+        if( $self->{image_src} ne $obj_ref->{image_src} )
+        {
+            $self->{image_src} = $obj_ref->{image_src};
+            $is_modified = 1;
+        }
     }
-    $self->{image_position} = $obj_ref->{image_position};
-    $self->{image_alt_text} = $obj_ref->{image_alt_text};
-    $self->{gift_card} = $obj_ref->{gift_card};
-    $self->{seo_title} = $obj_ref->{seo_title};
-    $self->{seo_description} = $obj_ref->{seo_description};
-    $self->{google_shopping_metafields} = $obj_ref->{google_shopping_metafields};
-    $self->{variant_image} = $obj_ref->{variant_image};
-    $self->{variant_weight_unit} = $obj_ref->{variant_weight_unit};
-    $self->{variant_tax_code_shopify_plus} = $obj_ref->{variant_tax_code_shopify_plus};
-    $self->{cost_per_item} = $obj_ref->{cost_per_item};
-    $self->{status} = $obj_ref->{status};
+    if( $self->{image_position} ne $obj_ref->{image_position} )
+    {
+        $self->{image_position} = $obj_ref->{image_position};
+        $is_modified = 1;
+    }
+    if( $self->{image_alt_text} ne $obj_ref->{image_alt_text} )
+    {
+        $self->{image_alt_text} = $obj_ref->{image_alt_text};
+        $is_modified = 1;
+    }
+    if( $self->{gift_card} ne $obj_ref->{gift_card} )
+    {
+        $self->{gift_card} = $obj_ref->{gift_card};
+        $is_modified = 1;
+    }
+    if( $self->{seo_title} ne $obj_ref->{seo_title} )
+    {
+        $self->{seo_title} = $obj_ref->{seo_title};
+        $is_modified = 1;
+    }
+    if( $self->{seo_description} ne $obj_ref->{seo_description} )
+    {
+        $self->{seo_description} = $obj_ref->{seo_description};
+        $is_modified = 1;
+    }
+    if( $self->{google_shopping_metafields} ne $obj_ref->{google_shopping_metafields} )
+    {
+        $self->{google_shopping_metafields} = $obj_ref->{google_shopping_metafields};
+        $is_modified = 1;
+    }
+    if( $self->{variant_image} ne $obj_ref->{variant_image} )
+    {
+        $self->{variant_image} = $obj_ref->{variant_image};
+        $is_modified = 1;
+    }
+    if( $self->{variant_weight_unit} ne $obj_ref->{variant_weight_unit} )
+    {
+        $self->{variant_weight_unit} = $obj_ref->{variant_weight_unit};
+        $is_modified = 1;
+    }
+    if( $self->{variant_tax_code_shopify_plus} ne $obj_ref->{variant_tax_code_shopify_plus} )
+    {
+        $self->{variant_tax_code_shopify_plus} = $obj_ref->{variant_tax_code_shopify_plus};
+        $is_modified = 1;
+    }
+    if( $self->{cost_per_item} != $obj_ref->{cost_per_item} )
+    {
+        $self->{cost_per_item} = $obj_ref->{cost_per_item};
+        $is_modified = 1;
+    }
+    if( $self->{status} ne $obj_ref->{status} )
+    {
+        $self->{status} = $obj_ref->{status};
+        $is_modified = 1;
+    }
+
+    return $is_modified;
 }
 
 sub set_status_active()
