2b24c4538cdaa2518866083f84178e3d65036a74
trodevel <trodevel@gmail.com>
2021-02-04 03:19:15 +0100

    extracted Helpers, ParserRewe

---
+++ Helpers.pm
@@ -0,0 +1,158 @@
+#!/usr/bin/perl
+
+#
+# Helpers.
+#
+# Copyright (C) 2021 Dr. Sergey Kolevatov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+package Helpers;
+
+use strict;
+use warnings;
+use utf8;
+
+sub sanitize_title($)
+{
+    my $title = shift;
+
+    $title =~ s/[\xc2\xad]/ /g;
+
+    return $title;
+}
+
+sub convert_title_to_handle($)
+{
+    my $title = shift;
+
+    my $res = lc $title;
+    $res =~ s/ /-/g;
+    $res =~ s/,/-/g;
+    $res =~ s/['"!\.&%´:\/()\+=®*²]//g;     #'
+    $res =~ s/ü/ue/g;
+    $res =~ s/ä/ae/g;
+    $res =~ s/ö/oe/g;
+    $res =~ s/ß/ss/g;
+    $res =~ s/β/ss/g;
+
+    $res =~ s/[àâá]/a/g;
+    $res =~ s/ç/c/g;
+    $res =~ s/[éèë]/e/g;
+    $res =~ s/ñ/n/g;
+    $res =~ s/å/ae/g;
+    $res =~ s/[ìîí]/i/g;
+    $res =~ s/ó/o/g;
+    $res =~ s/[úû]/u/g;
+
+    $res =~ s/--/-/g;
+    $res =~ s/--/-/g;
+    $res =~ s/-$//g;
+
+    return $res;
+}
+
+sub parse_price($)
+{
+    my $price = shift;
+
+    my $res = $price;
+
+    $res =~ s/€//g;
+    $res =~ s/,/./;
+    $res += 0.0;
+
+    return $res;
+}
+
+sub round_up_price($)
+{
+    my ( $price ) = @_;
+
+    my $fixed_point_price = int( $price * 100 );
+
+    my $last_digit = $fixed_point_price % 10;
+
+    my $delta = 0;
+
+    if( $last_digit > 0 )
+    {
+        if( $last_digit > 5 )
+        {
+            $delta = 9 - $last_digit;
+        }
+        else
+        {
+            $delta = 5 - $last_digit;
+        }
+    }
+
+    my $res = $price + ( $delta / 100 );
+
+    return $res;
+}
+
+sub apply_price_factor($$$)
+{
+    my ( $price, $price_factor, $should_round_up ) = @_;
+
+    my $res = $price * $price_factor;
+
+    $res = sprintf( "%.2f", $res );
+
+    $res += 0.0;
+
+    $res = ( $should_round_up == 1 ) ? round_up_price( $res ) : $res;
+
+    return $res;
+}
+
+sub is_handle_unique($$)
+{
+    my ( $handles_ref, $handle ) = @_;
+
+    if( exists( $handles_ref->{ $handle } ) )
+    {
+        $handles_ref->{ $handle }++;
+
+        return 0;
+    }
+
+    $handles_ref->{ $handle } = 1;
+
+    return 1;
+}
+
+sub add_or_update_categories($$$)
+{
+    my ( $categories_ref, $category, $sub_category ) = @_;
+
+    if( exists $categories_ref->{$category} )
+    {
+        if( not exists $categories_ref->{$category}->{$sub_category} )
+        {
+            $categories_ref->{$category}->{$sub_category} = 1;
+        }
+    }
+    else
+    {
+        my %hash;
+        $categories_ref->{$category} = \%hash;
+
+        $categories_ref->{$category}->{$sub_category} = 1;
+    }
+}
+
+1;
---
+++ ParserRewe.pm
@@ -0,0 +1,138 @@
+#!/usr/bin/perl
+
+#
+# Rewe Parser.
+#
+# Copyright (C) 2021 Dr. Sergey Kolevatov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+package ParserRewe;
+
+use strict;
+use warnings;
+use utf8;
+
+require Product;
+
+sub convert_to_tag($)
+{
+    my $s = shift;
+
+    my $res = $s;
+    $res =~ s/,//g;
+    $res =~ s/"//g; #"
+
+    return $res;
+}
+
+sub parse_pic($)
+{
+    my $pic = shift;
+
+    my $res = $pic;
+
+    $res =~ s/resize=152px:152px&//;
+    $res =~ s/\?output-quality=80&output-format=jpeg//;
+
+    return $res;
+}
+
+sub conv_fields_to_shopify($$$$$$)
+{
+    my ( $fields_ref, $handles_ref, $categories_ref, $vendor_id, $price_factor, $should_round_up ) = @_;
+
+    my @fields = @{ $fields_ref };
+
+    #my $num_fields = scalar @fields; # DEBUG
+
+    #print "DEBUG: fields=$num_fields\n";
+
+    my $handle = Helpers::convert_title_to_handle( $fields[4] );
+
+    if( Helpers::is_handle_unique( $handles_ref, $handle ) == 0 )
+    {
+        print "WARNING: duplicate handle '$handle'\n";
+        return;
+    }
+
+    my $title  = Helpers::sanitize_title( $fields[4] );
+
+    my $tag_1  = convert_to_tag( $fields[2] );
+    my $tag_2  = convert_to_tag( $fields[3] );
+
+    my $tags = '"' . $tag_1 ."," . $tag_2 . '"';
+
+    Helpers::add_or_update_categories( $categories_ref, $tag_1, $tag_2 );
+
+    my $cost_per_item  = Helpers::parse_price( $fields[6] );
+
+    if( $cost_per_item == -1 )
+    {
+        $cost_per_item  = Helpers::parse_price( $fields[8] );
+    }
+
+    my $price = Helpers::apply_price_factor( $cost_per_item, $price_factor, $should_round_up );
+
+    my $pic  = parse_pic( $fields[10] );
+
+    my $product = new Product(
+        $handle,
+        $title,
+        '', #  body_html
+        $vendor_id,
+        $fields[1], # type
+        $tags, # tags
+        'TRUE', # published
+        'Title', # option1_name
+        'Default Title', # option1_value
+        '', # option2_name
+        '', # option2_value
+        '', # option3_name
+        '', # option3_value
+        '', # variant_sku
+        '0', # variant_grams
+        '', # variant_inventory_tracker
+        '', # variant_inventory_qty
+        'deny', # variant_inventory_policy
+        'manual', # variant_fulfillment_service
+        $price, # variant_price
+        '', # variant_compare_at_price
+        'TRUE', # variant_requires_shipping
+        '', # variant_taxable
+        '', # variant_barcode
+        $pic, # image_src
+        '', # image_position
+        '', # image_alt_text
+        'FALSE', # gift_card
+        '', # seo_title
+        '', # seo_description
+        '', # google_shopping_metafields
+        '', # variant_image
+        '', # variant_weight_unit
+        '', # variant_tax_code_shopify_plus
+        $cost_per_item, # cost_per_item
+        'active' # status
+        );
+
+    #print $handle, ",", $title, ",", $price,",", $pic, "\n";
+
+    # Shopify fields
+    # https://help.shopify.com/en/manual/products/import-export/using-csv#product-csv-file-format
+
+    print OUTPUT $product->to_csv() . "\n";
+}
+
+1;
